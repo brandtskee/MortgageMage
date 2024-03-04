93c80a0a2902e553fe976e59314ab4c0e8977887
brandtskee <brandtske@gmail.com>
2024-03-04 11:28:02 -0700

    Implement walking and idle animation (walking is kind of broken)

--- characters/main_character.gd
+++ characters/main_character.gd
@@ -1,6 +1,16 @@
 extends CharacterBody2D
 
 @export var move_speed: float = 100
+@export var starting_direction: Vector2 = Vector2(0, 0.1)
+
+# parameters/Idle/blend_position
+
+# get reference to animation tree from scence
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+func _ready():
+	update_animation_parameters(starting_direction)
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
@@ -8,8 +18,25 @@ func _physics_process(_delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
+	update_animation_parameters(input_direction)
+	
 	# updated velocity
 	velocity = input_direction*move_speed
 	
-	# move and slide (USE THIS FOR MOVEMENT!!)
+	# move and slide (USE THIS FOR MOVEMENT!!) 
 	move_and_slide()
+	
+	# pick new animation state
+	pick_new_state()
+
+func update_animation_parameters(move_input: Vector2):
+	if (move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+
+# choose animation state
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("walk", false)
+	else:
+		state_machine.travel("Idle", false)
