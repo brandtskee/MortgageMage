87e508e8f3c3582d5347b555a0a83996f0b9461d
brandtskee <brandtske@gmail.com>
2024-03-18 09:51:13 -0600

    Added enemy attack and animations

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -22,6 +22,18 @@ func _ready():
 	await textbox_closed
 	$ActionsPanel.show()
 
+# use this function after displaying textbox to allow for pressing action button without
+# accidentally selecting the previous action again
+func deselect_actions():
+	$ActionsPanel/HBoxContainer/Attack.hide()
+	$ActionsPanel/HBoxContainer/Attack.show()
+	
+	$ActionsPanel/HBoxContainer/Defend.hide()
+	$ActionsPanel/HBoxContainer/Defend.show()
+	
+	$ActionsPanel/HBoxContainer/Run.hide()
+	$ActionsPanel/HBoxContainer/Run.show()
+
 func set_health(progress_bar, health, max_health):
 	progress_bar.value = health
 	progress_bar.max_value = max_health
@@ -41,16 +53,33 @@ func display_text(text):
 func _on_run_pressed():
 	display_text("Can't flee this fight!")
 	# rapidly hide and show button to defocus so that enter and space can be used
-	$ActionsPanel/HBoxContainer/Run.hide()
-	$ActionsPanel/HBoxContainer/Run.show()
+	deselect_actions()
 	await textbox_closed
 
+func enemy_turn():
+	display_text("%s attempts to extort you for more money!" % enemy.name)
+	deselect_actions()
+	await textbox_closed
+	# use max function to ensure health does not go below zero
+	current_player_health = max(0, current_player_health - enemy.damage)
+	set_health($PlayerPanel/PlayerData/ProgressBar, current_player_health, State.max_health)
+	$AnimationPlayer.play("shake")
+	display_text("%s dealt %d damage!" % [enemy.name, enemy.damage])
+	await textbox_closed
 
 func _on_attack_pressed():
 	display_text("You attack and do a bit of damage!")
-	$ActionsPanel/HBoxContainer/Attack.hide()
-	$ActionsPanel/HBoxContainer/Attack.show()
+	deselect_actions()
 	await textbox_closed
 	# use max function to ensure health does not go below zero
 	current_enemy_health = max(0, current_enemy_health - State.damage)
 	set_health($EnemyContainer/ProgressBar, current_enemy_health, enemy.health)
+	# play enemy damaged animation
+	$AnimationPlayer.play("enemy_damaged")
+	await "animation_finished"
+	
+	display_text("You did %s damage!" % State.damage)
+	await textbox_closed
+	
+	# execute enemy turn after your attack
+	enemy_turn()
