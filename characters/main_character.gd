32a077b8192aba67faca43ebfef3ec8548947b8e
brandtskee <brandtske@gmail.com>
2024-03-19 13:47:57 -0600

    Added quotes from the loanshark prior to battle

--- characters/main_character.gd
+++ characters/main_character.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+signal textbox_closed
 
 @export var move_speed: float = 100
 @export var starting_direction: Vector2 = Vector2(0, 1)
@@ -11,6 +12,16 @@ extends CharacterBody2D
 
 func _ready():
 	update_animation_parameters(starting_direction)
+	$Textbox.hide()
+	
+func display_text(text):
+	$Textbox.show()
+	$Textbox/Label.text = text
+	
+func _input(event):
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)) and $Textbox.visible:
+		$Textbox.hide()
+		emit_signal("textbox_closed")
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
@@ -66,6 +77,13 @@ func _on_apartment_door_area_entered(area):
 func _on_shark_boss_area_entered(area):
 	# if player collides with area2d in mainOutside, change scene to shark fight
 	print("Entered")
+	display_text("'You think you can show your face\naround here?'")
+	await textbox_closed
+	display_text("'You owe me four thousand dollars in\ninterest fees!'")
+	await textbox_closed
+	display_text("The loanshark appears to be preparing\nto fight...")
+	await textbox_closed
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://scenes/battle.tscn")
 	# How do we get it so area2d is disabled after boss dies?
 
