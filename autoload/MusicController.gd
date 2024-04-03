21f196bd75cc765edf9d0005c1131da9e53dad5e
brandtskee <brandtske@gmail.com>
2024-04-03 13:00:29 -0600

    Changed desert entrance point and adjusted music and goblin size

--- autoload/MusicController.gd
+++ autoload/MusicController.gd
@@ -5,12 +5,12 @@ var battle_music = load("res://assets/audio/battleMUSIC.mp3")
 
 func play_background():
 	$Music.stream = background_music
-	$Music.volume_db = -15
+	$Music.volume_db = -5
 	$Music.play()
 	
 func play_battle():
 	$Music.stream = battle_music
-	$Music.volume_db = -15
+	$Music.volume_db = -5
 	$Music.play()
 
 func stop():
