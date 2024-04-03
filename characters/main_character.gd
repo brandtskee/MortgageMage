383f27e28be5fb24a527bc908ce3973f3ff84014
Yasir <yasirrjami@hotmail.com>
2024-04-03 03:40:43 -0600

    Update main outside and add goblin

--- characters/main_character.gd
+++ characters/main_character.gd
@@ -106,6 +106,34 @@ func _on_shark_boss_area_entered(area):
 		# How do we get it so area2d is disabled after boss dies?
 		get_node("CollisionShape2D").disabled = true
 
+func _on_goblin_boss_area_entered(area):
+	# if player collides with area2d in mainDesert, change scene to goblin fight
+	# disable movement and set idle animation before battle starts
+	
+	# logic if previous battle is WON
+	if State.goblin_battle == 1:
+		set_physics_process(false)
+		display_text("'Kids these days.\nNo respect for their elders...'")
+		await textbox_closed
+		set_physics_process(true)
+		State.goblin_battle = 2
+	
+	# logic if battle has not occurred yet
+	if State.goblin_battle == 0:
+		MusicController.stop()
+		set_physics_process(false)
+		state_machine.travel("Idle", false)
+		print("Entered")
+		display_text("'Hey, punk. You looking for trouble?\n'Cause I'm trouble.'")
+		await textbox_closed
+		display_text("'I don't got time for you.\nI'll make this quick.'")
+		await textbox_closed
+		await get_tree().create_timer(0.5).timeout
+		State.previous_scene.pack(get_tree().get_current_scene())
+		MusicController.play_battle()
+		get_tree().change_scene_to_file("res://scenes/battleGoblin.tscn")
+		get_node("CollisionShape2D").disabled = true
+
 func _on_desert_area_entered(area):
 	# Change to desert scene
 	get_tree().change_scene_to_file("res://scenes/mainDesert.tscn")
@@ -118,7 +146,6 @@ func set_health(progress_bar, health, max_health):
 func _on_desert_to_outside_area_entered(area):
 	get_tree().change_scene_to_file("res://scenes/mainOutside.tscn")
 
-
 func _on_tutorial_area_area_entered(area):
 	if State.tutorial_passed == 0:
 		MusicController.play_background()
@@ -129,3 +156,4 @@ func _on_tutorial_area_area_entered(area):
 		await textbox_closed
 		set_physics_process(true)
 		State.tutorial_passed = 1
+
